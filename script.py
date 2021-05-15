a55a4f550a21360699eedd69330d7511bf67be29
dtfabio <dtfabio96@gmail.com>
2021-05-15 20:10:59 +0200

    Update3

--- script.py
+++ script.py
@@ -17,8 +17,7 @@ def readerbin(dir_path):
         binlist.append(line.replace('\n',''))
     return binlist
 
-def checkbin(filelist, binlist):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+def checkbin_pb(filelist, binlist, dir_path):
     with open(dir_path + "/Output/card_report.csv", 'w') as out_file:                   
         writer = csv.writer(out_file)
         writer.writerow(('Card Number', 'Expiry Date (Month)', 'Expiry Date (Year)', 'CVV', 'Holder', 'Bank'))
@@ -29,12 +28,25 @@ def checkbin(filelist, binlist):
                     card = fileitem.split("|")      
                     writer.writerow(card)
 
+def checkbin_src(filelist, binlist, dir_path):
+    with open(dir_path + "/Output/raw_with_bin.csv", 'w') as out_file:                   
+        write = csv.writer(out_file)        
+        for fileitem in filelist:
+            for binitem in binlist:
+                if binitem in fileitem:
+                    card = fileitem.split("\n") 
+                    write.writerow(card)
+
 try:
     dir_path = os.path.dirname(os.path.realpath(__file__))
     filelist=readerfile(sys.argv[1], dir_path)
     binlist=readerbin(dir_path)
-    checkbin(filelist, binlist)
-    print("Completed.")
+    if(sys.argv[2]=='-pb1'):
+        checkbin_pb(filelist, binlist, dir_path)
+        print("Completed.")
+    elif(sys.argv[2]=='-src'):
+        checkbin_src(filelist, binlist, dir_path)
+        print("Completed.")
 except:
     print("Error.")
 
